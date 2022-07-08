c6e2cbc23238b4b7a90b6e06bfcf00d7dd03e12a
goto <kenta_goto@mates-system.com>
2022-07-09 04:54:24 +0900

    WIP: style: todo comments

--- lib/waf-cdk-ip-restrictions-stack.ts
+++ lib/waf-cdk-ip-restrictions-stack.ts
@@ -68,13 +68,14 @@ export class WafCdkIpRestrictionsStack extends Stack {
     const lines = ipListFile.toString().split("\r\n");
 
     for (const line of lines) {
+      // TODO: check CIDR format or not (/32, /16, etc...)
       const trimmedLine = line
         .replace(/ /g, "")
         .replace(/\t/g, "")
         .replace(/\n/g, "")
         .replace(/^([^#]+)#.*$/g, "$1");
 
-      // TODO: check empty lines whether or not (need \n??)
+      // TODO: check empty lines whether or not (need \n handlings??)
       const pattern = /^#/g;
       const result = trimmedLine.match(pattern);
 
