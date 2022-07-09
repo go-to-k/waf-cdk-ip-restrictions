4c4efd2bc6eb60248ea04e9f2cd4820b60bd9cbf
go-to-k <24818752+go-to-k@users.noreply.github.com>
2022-07-10 03:36:57 +0900

    test: add basic unit tests

--- test/waf-cdk-ip-restrictions.test.ts
+++ test/waf-cdk-ip-restrictions.test.ts
@@ -1,17 +1,38 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as WafCdkIpRestrictions from '../lib/waf-cdk-ip-restrictions-stack';
+import * as cdk from "aws-cdk-lib";
+import { Match, Template } from "aws-cdk-lib/assertions";
+import { WafCdkIpRestrictionsStack } from "../lib/resource/waf-cdk-ip-restrictions-stack";
+import _cdkJsonRaw from "../cdk.json";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/waf-cdk-ip-restrictions-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new WafCdkIpRestrictions.WafCdkIpRestrictionsStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+export type CdkJson = typeof _cdkJsonRaw;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+const _cdkJson: CdkJson = _cdkJsonRaw;
+const app = new cdk.App({
+  context: _cdkJson.context,
+});
+const region = app.node.tryGetContext("region") ?? "";
+const stack = new WafCdkIpRestrictionsStack(app, "WafCdkIpRestrictionsStack", {
+  env: {
+    region: region,
+  },
+});
+const template = Template.fromStack(stack);
+
+test("WebACL created", () => {
+  template.resourceCountIs("AWS::WAFv2::WebACL", 1);
+});
+
+test("IPSet created", () => {
+  template.resourceCountIs("AWS::WAFv2::IPSet", 1);
+});
+
+test("WAF default action is block", () => {
+  template.hasResourceProperties("AWS::WAFv2::WebACL", {
+    DefaultAction: { Block: {} },
+  });
+});
+
+test("IPSet has addresses", () => {
+  template.hasResourceProperties("AWS::WAFv2::IPSet", {
+    Addresses: Match.anyValue(),
+  });
 });
