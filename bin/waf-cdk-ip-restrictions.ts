86b9ddac56853fec8c9b50c6a759b942da61087e
goto <kenta_goto@mates-system.com>
2022-07-09 15:43:32 +0900

    feat: validator and refactoring

--- bin/waf-cdk-ip-restrictions.ts
+++ bin/waf-cdk-ip-restrictions.ts
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { WafCdkIpRestrictionsStack } from "../lib/waf-cdk-ip-restrictions-stack";
-
-const region = process.env.REGION ?? "";
+import { WafCdkIpRestrictionsStack } from "../lib/resource/waf-cdk-ip-restrictions-stack";
 
 const app = new cdk.App();
+
+const region = app.node.tryGetContext("region") ?? "";
+
 new WafCdkIpRestrictionsStack(app, "WafCdkIpRestrictionsStack", {
   env: {
     region: region,
