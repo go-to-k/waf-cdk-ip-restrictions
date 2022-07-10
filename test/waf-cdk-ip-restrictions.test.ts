01c3797cba6b42686e4867fd5e017282c5e0eebd
goto <kenta_goto@mates-system.com>
2022-07-10 19:10:00 +0900

    test: add test cases for validations and snapshots

--- test/waf-cdk-ip-restrictions.test.ts
+++ test/waf-cdk-ip-restrictions.test.ts
@@ -5,34 +5,97 @@ import _cdkJsonRaw from "../cdk.json";
 
 export type CdkJson = typeof _cdkJsonRaw;
 
-const _cdkJson: CdkJson = _cdkJsonRaw;
-const app = new cdk.App({
-  context: _cdkJson.context,
-});
-const region = app.node.tryGetContext("region") ?? "";
-const stack = new WafCdkIpRestrictionsStack(app, "WafCdkIpRestrictionsStack", {
-  env: {
-    region: region,
-  },
-});
-const template = Template.fromStack(stack);
+const getTemplate = (region?: string, scopeType?: string): cdk.assertions.Template => {
+  const _cdkJson: CdkJson = _cdkJsonRaw;
 
-test("WebACL created", () => {
-  template.resourceCountIs("AWS::WAFv2::WebACL", 1);
-});
+  if (region !== undefined) _cdkJson.context.region = region;
+  if (scopeType !== undefined) _cdkJson.context.scopeType = scopeType;
 
-test("IPSet created", () => {
-  template.resourceCountIs("AWS::WAFv2::IPSet", 1);
-});
+  const app = new cdk.App({
+    context: _cdkJson.context,
+  });
+  const regionContext = app.node.tryGetContext("region") ?? "";
+  const stack = new WafCdkIpRestrictionsStack(app, "WafCdkIpRestrictionsStack", {
+    env: {
+      region: regionContext,
+    },
+  });
+  const template = Template.fromStack(stack);
+  return template;
+};
 
-test("WAF default action is block", () => {
-  template.hasResourceProperties("AWS::WAFv2::WebACL", {
-    DefaultAction: { Block: {} },
+describe("Snapshot Tests", () => {
+  const template = getTemplate();
+
+  test("Snapshot test", () => {
+    expect(template.toJSON()).toMatchSnapshot();
   });
 });
 
-test("IPSet has addresses", () => {
-  template.hasResourceProperties("AWS::WAFv2::IPSet", {
-    Addresses: Match.anyValue(),
+describe("Fine-grained Assertions Tests", () => {
+  const template = getTemplate();
+
+  test("WebACL created", () => {
+    template.resourceCountIs("AWS::WAFv2::WebACL", 1);
+  });
+
+  test("IPSet created", () => {
+    template.resourceCountIs("AWS::WAFv2::IPSet", 1);
+  });
+
+  test("WAF default action is block", () => {
+    template.hasResourceProperties("AWS::WAFv2::WebACL", {
+      DefaultAction: { Block: {} },
+    });
+  });
+
+  test("IPSet has addresses", () => {
+    template.hasResourceProperties("AWS::WAFv2::IPSet", {
+      Addresses: Match.anyValue(),
+    });
   });
 });
+
+describe("Validation Tests", () => {
+  test("SCOPE: CLOUDFRONT - REGION: ap-northeast-1 = Error", () => {
+    const region = "ap-northeast-1";
+    const scopeType = "CLOUDFRONT";
+    expect(() => {
+      const template = getTemplate(region, scopeType);
+    }).toThrowError(/Region must be us-east-1 when CLOUDFRONT./);
+  });
+
+  test("SCOPE: CLOUDFRONT - REGION: us-east-1 = Success", () => {
+    const region = "us-east-1";
+    const scopeType = "CLOUDFRONT";
+    expect(() => {
+      const template = getTemplate(region, scopeType);
+    }).not.toThrow(Error);
+  });
+
+  test("SCOPE: REGIONAL - REGION: ap-northeast-1 = Success", () => {
+    const region = "ap-northeast-1";
+    const scopeType = "REGIONAL";
+    expect(() => {
+      const template = getTemplate(region, scopeType);
+    }).not.toThrow(Error);
+  });
+
+  test("SCOPE: REGIONAL - REGION: us-east-1 = Success", () => {
+    const region = "us-east-1";
+    const scopeType = "REGIONAL";
+    expect(() => {
+      const template = getTemplate(region, scopeType);
+    }).not.toThrow(Error);
+  });
+
+  test("SCOPE: INVALID - REGION: us-east-1 = Error", () => {
+    const region = "us-east-1";
+    const scopeType = "INVALID";
+    expect(() => {
+      const template = getTemplate(region, scopeType);
+    }).toThrowError(/Scope must be CLOUDFRONT or REGIONAL./);
+  });
+});
+
+describe("IP List Tests", () => {});
